b99203c85d406aa729ffd7be2aee58792a0920a0
anjaan7664 <mukeshsoni73362@gmail.com>
2018-06-19 12:43:36 +0530

    Change

--- app/src/main/java/satlaa/desijewellery/MainActivity.java
+++ app/src/main/java/satlaa/desijewellery/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-//Test comment
 
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},

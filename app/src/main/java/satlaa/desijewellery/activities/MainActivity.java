5233874b68632451d007d0d069c9b4c83cc0d88e
anjaan7664 <mukeshsoni73362@gmail.com>
2018-07-29 10:50:13 +0530

    Before adView in recyclerview

--- app/src/main/java/satlaa/desijewellery/activities/MainActivity.java
+++ app/src/main/java/satlaa/desijewellery/activities/MainActivity.java
@@ -2,15 +2,17 @@ package satlaa.desijewellery.activities;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
@@ -25,8 +27,16 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
+import java.util.HashMap;
+
+import satlaa.desijewellery.BuildConfig;
 import satlaa.desijewellery.helper_activity.Setting;
 import satlaa.desijewellery.R;
 import satlaa.desijewellery.helper_activity.Webview;
@@ -34,6 +44,8 @@ import satlaa.desijewellery.utils.LocaleHelper;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -46,6 +58,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
+        HashMap<String, Object> defaults = new HashMap<>();
+        defaults.put("app_version", 12);
+        mFirebaseRemoteConfig.setDefaults(defaults);
+        fetchAppVersion();
 
         ImageView website = findViewById(R.id.website);
         website.setOnClickListener(new View.OnClickListener() {
@@ -65,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         mInterstitialAd.setAdUnitId("ca-app-pub-9733613923055204/8441414611");
         AdRequest adRequest = new AdRequest.Builder().build();
         mInterstitialAd.loadAd(adRequest);
-        button = (Button) findViewById(R.id.btn_desi);
+        button = findViewById(R.id.btn_desi);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button = (Button) findViewById(R.id.btn_emboss);
+        button = findViewById(R.id.btn_emboss);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        button = (Button) findViewById(R.id.btn_about_us);
+        button = findViewById(R.id.btn_about_us);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -124,8 +142,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         isStoragePermissionGranted();
-
     }
+
+
 
 
     @Override
@@ -221,8 +240,63 @@ public class MainActivity extends AppCompatActivity {
     // Loading ad for DJphotos so it directly show without wasting time and sending it to Djphotos when method call
     public static void loadAd(View view) {
         AdView mAdView;
-        mAdView = (AdView) view.findViewById(R.id.adView);
+        mAdView = view.findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+    }
+
+    private void fetchAppVersion() {
+
+        long cacheExpiration = 86400; //for 1 day.
+
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+                        }
+                        displayWelcomeMessage();
+                    }
+                });
+    }
+
+    private void displayWelcomeMessage() {
+        int verCode = 0;
+        try {
+            PackageInfo pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
+            verCode = pInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int app_version = (int) mFirebaseRemoteConfig.getLong("app_version");
+        if (app_version > verCode) {
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle(R.string.new_version)
+                    .setMessage(R.string.new_version_dsc)
+                    .setPositiveButton(R.string.update,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                                    try {
+                                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                                    } catch (android.content.ActivityNotFoundException anfe) {
+                                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                                    }
+
+                                }
+                            }).setNegativeButton(R.string.no_thanks,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            }).create();
+            dialog.show();
+        }
+
     }
 }
